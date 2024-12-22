373040d535cc600fef4e8ac435feaeb8d9902afc
RAT <rat@RAT>
2024-12-22 09:42:49 +0500

    update with new features

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "https://expense-tracker-4cc8b-default-rtdb.firebaseio.com/",
+  databaseURL: "https://expense-tracker-b1317-default-rtdb.firebaseio.com/",
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_STORAGE_BUCKET",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
